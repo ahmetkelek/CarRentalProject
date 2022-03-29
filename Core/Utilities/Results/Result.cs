eb428cb45760ac841cd3fbae7c883825307506b3
Ahmet Kelek <ahmet.kelek10@gmail.com>
2022-03-29 23:02:45 +0300

    Results and Interceptors added

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //private bool v1;
+        //private string v2;
+        //gerek yok bunlara suanlik
+
+
+        //get read-only'dir. Read-only'ler constructurda set edilebilir
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+            //Success = success; alttaki success'ida kapladigi icin bunu siliyoruz
+
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
---
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessDataResult<T>:DataResult<T>
+    {
+        public SuccessDataResult(T data,string message):base(data,true,message)
+        {
+
+        }
+
+        public SuccessDataResult(T data):base(data,true)
+        {
+
+        }
+
+        //genelde alttaki ikisi cok kullanilmaz ama alternatif gosteriilir
+        public SuccessDataResult(string message):base(default,true)
+        {
+
+        }
+
+        public SuccessDataResult():base(default,true)
+        {
+
+        }
+    }
+}
